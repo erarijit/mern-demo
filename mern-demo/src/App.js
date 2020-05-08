f15e074db6d01b7f1711407fb9c3b9cb1f919c05
erarijit <arijitmukherjee1988@gmail.com>
2020-05-08 19:12:20 +0530

    Bootstrap Add

--- mern-demo/src/App.js
+++ mern-demo/src/App.js
@@ -1,26 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. My First React App.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lets Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="container">
+        <h2>MERN Stack CRUD Operation</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
