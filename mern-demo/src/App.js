14a3076a34c60413c6f019673827ed37b8a7ed97
erarijit <arijitmukherjee1988@gmail.com>
2020-05-08 19:19:44 +0530

    Bootstrap Added

--- mern-demo/src/App.js
+++ mern-demo/src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
 //import "./App.css";
 
-function App() {
+class App extends Component {
   render() {
     return (
       <div className="container">
